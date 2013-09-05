c8f9bac0ec09ffea3dd0ec005443a041995de295
marselester <marselester@ya.ru>
2013-09-05 16:40:54 +0700

    Add Django middleware

---
+++ pyodnoklassniki/contrib/django/middleware.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+"""
+Django PyOdnoklassniki Middleware.
+
+In order to install it add the following to your ``settings.py``:
+
+- 'pyodnoklassniki.contrib.django.middleware.PyOdnoklassnikiMiddleware'
+  to ``MIDDLEWARE_CLASSES``;
+- Odnoklassniki application's credentials::
+
+      PYODNOKLASSNIKI = {
+          'app_pub_key': 'CBAJ...BABA',
+          'app_secret_key': '123...XYZ',
+      }
+
+"""
+import pyodnoklassniki
+from django.core.exceptions import MiddlewareNotUsed
+from django.conf import settings
+
+
+class PyOdnoklassnikiMiddleware(object):
+
+    def __init__(self):
+        ok_settings = getattr(settings, 'PYODNOKLASSNIKI', {})
+
+        if not ok_settings.get('app_pub_key'):
+            raise MiddlewareNotUsed
+
+        if not ok_settings.get('app_secret_key'):
+            raise MiddlewareNotUsed
+
+        pyodnoklassniki.app_pub_key = ok_settings['app_pub_key']
+        pyodnoklassniki.app_secret_key = ok_settings['app_secret_key']
