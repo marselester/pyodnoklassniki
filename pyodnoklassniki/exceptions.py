07c846d81d4e48efec6d01c09849df076201b5e9
marselester <marselester@ya.ru>
2013-09-15 19:12:35 +0700

    Add error constants

--- pyodnoklassniki/exceptions.py
+++ pyodnoklassniki/exceptions.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from . import errors
 
 
 class OdnoklassnikiError(Exception):
@@ -13,11 +14,11 @@ class APIError(OdnoklassnikiError):
     """API server errors, e.g., specific error code or invalid response object.
     """
 
-    ERROR_CODES = {
-        1: 'UNKNOWN',
-        2: 'SERVICE',
-        9999: 'SYSTEM',
-    }
+    ERROR_CODES = (
+        errors.UNKNOWN,
+        errors.SERVICE,
+        errors.SYSTEM,
+    )
 
     def __init__(self, message, http_content, http_status_code, json=None):
         super(APIError, self).__init__(message)
@@ -30,24 +31,24 @@ class APIError(OdnoklassnikiError):
 class AuthError(OdnoklassnikiError):
     """Authentication and authorization errors."""
 
-    ERROR_CODES = {
-        7: 'ACTION_BLOCKED',
-        8: 'FLOOD_BLOCKED',
-        9: 'IP_BLOCKED',
-        10: 'PERMISSION_DENIED',
-        11: 'LIMIT_REACHED',
-        101: 'PARAM_API_KEY',
-        102: 'PARAM_SESSION_EXPIRED',
-        103: 'PARAM_SESSION_KEY',
-        104: 'PARAM_SIGNATURE',
-        105: 'PARAM_RESIGNATURE',
-        200: 'PARAM_PERMISSION',
-        210: 'PARAM_APPLICATION_DISABLED',
-        401: 'AUTH_LOGIN',
-        402: 'AUTH_LOGIN_CAPTCHA',
-        453: 'SESSION_REQUIRED',
-        455: 'FRIEND_RESTRICTION',
-    }
+    ERROR_CODES = (
+        errors.ACTION_BLOCKED,
+        errors.FLOOD_BLOCKED,
+        errors.IP_BLOCKED,
+        errors.PERMISSION_DENIED,
+        errors.LIMIT_REACHED,
+        errors.PARAM_API_KEY,
+        errors.PARAM_SESSION_EXPIRED,
+        errors.PARAM_SESSION_KEY,
+        errors.PARAM_SIGNATURE,
+        errors.PARAM_RESIGNATURE,
+        errors.PARAM_PERMISSION,
+        errors.PARAM_APPLICATION_DISABLED,
+        errors.AUTH_LOGIN,
+        errors.AUTH_LOGIN_CAPTCHA,
+        errors.SESSION_REQUIRED,
+        errors.FRIEND_RESTRICTION,
+    )
 
     def __init__(self, message, http_content=None, http_status_code=None, json=None):
         super(AuthError, self).__init__(message)
@@ -60,29 +61,29 @@ class AuthError(OdnoklassnikiError):
 class InvalidRequestError(OdnoklassnikiError, ValueError):
     """Invalid request parameters' errors."""
 
-    ERROR_CODES = {
-        3: 'METHOD',
-        4: 'REQUEST',
-        21: 'NOT_MULTIPART',
-        100: 'PARAM',
-        110: 'PARAM_USER_ID',
-        120: 'PARAM_ALBUM_ID',
-        130: 'PARAM_WIDGET',
-        140: 'PARAM_MESSAGE_ID',
-        300: 'NOT_FOUND',
-        324: 'EDIT_PHOTO_FILE',
-        454: 'CENSOR_MATCH',
-        500: 'PHOTO_SIZE_LIMIT_EXCEEDED',
-        501: 'PHOTO_SIZE_TOO_SMALL',
-        502: 'PHOTO_SIZE_TOO_BIG',
-        503: 'PHOTO_INVALID_FORMAT',
-        504: 'PHOTO_IMAGE_CORRUPTED',
-        505: 'PHOTO_NO_IMAGE',
-        900: 'NO_SUCH_APP',
-        1001: 'CALLBACK_INVALID_PAYMENT',
-        1002: 'PAYMENT_IS_REQUIRED_PAYMENT',
-        1003: 'INVALID_PAYMENT',
-    }
+    ERROR_CODES = (
+        errors.METHOD,
+        errors.REQUEST,
+        errors.NOT_MULTIPART,
+        errors.PARAM,
+        errors.PARAM_USER_ID,
+        errors.PARAM_ALBUM_ID,
+        errors.PARAM_WIDGET,
+        errors.PARAM_MESSAGE_ID,
+        errors.NOT_FOUND,
+        errors.EDIT_PHOTO_FILE,
+        errors.CENSOR_MATCH,
+        errors.PHOTO_SIZE_LIMIT_EXCEEDED,
+        errors.PHOTO_SIZE_TOO_SMALL,
+        errors.PHOTO_SIZE_TOO_BIG,
+        errors.PHOTO_INVALID_FORMAT,
+        errors.PHOTO_IMAGE_CORRUPTED,
+        errors.PHOTO_NO_IMAGE,
+        errors.NO_SUCH_APP,
+        errors.CALLBACK_INVALID_PAYMENT,
+        errors.PAYMENT_IS_REQUIRED_PAYMENT,
+        errors.INVALID_PAYMENT,
+    )
 
     def __init__(self, message, http_content=None, http_status_code=None, json=None):
         super(InvalidRequestError, self).__init__(message)
