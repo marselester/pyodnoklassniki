05d6ed45634080a6042188cb0494e1259d286a56
marselester <marselester@ya.ru>
2013-09-18 13:17:38 +0700

    Bump version to 0.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='pyodnoklassniki',
-    version='0.11',
+    version='0.12',
     packages=[
         'pyodnoklassniki',
         'pyodnoklassniki.contrib',
